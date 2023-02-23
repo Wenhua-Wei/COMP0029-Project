e1a2cf550c756429278340fda0751f693f08d349
Wenhua Wei <zczleix@ucl.ac.uk>
2023-02-23 16:53:43 +0000

    Randomly select subsets

---
+++ deep-BAS-subsets/config.py
@@ -0,0 +1,4 @@
+data_folder1 = '/Users/weiwenhua/UGY4/COMP0029/HCPData/100307'
+data_folder2 = '/Users/weiwenhua/anaconda3/envs/deepNODDIEnv/lib/python3.10/site-packages/dmipy/data/hcp'
+
+batch_size = 128
